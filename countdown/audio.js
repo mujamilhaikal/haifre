e900d6e8fec76beba6e45a4df4a4fa013f6a64a9
mujamilhaikal <haikal.mujamil.id@gmail.com>
2023-07-25 13:12:08 +0700

    recreate

--- countdown/audio.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var audio = document.getElementById("audio");
-var playButton = document.getElementById("playButton");
-
-playButton.addEventListener("click", function () {
-  audio.play();
-  playButton.classList.add("hide-button");
-});
